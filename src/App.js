b807649f9a830d23ddffadc56918bce7b0c68823
seaborisrc@gmail.com <seaborisrc@gmail.com>
2021-02-05 13:45:18 -0400

    Primer Commit Componente Curso JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/styles.scss";
+import Curso from "./Curso";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ()=>(
+  <>
+    <div className="main-banner img-container l-section" id="main-banner">
+    <div className="ed-grid lg-grid-6">
+      <div className="lg-cols-4 lg-x-2">
+        <img className="main-banner__img" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.dBcp1fg2jNxu7nPn6g1CfwHaEK%26pid%3DApi&f=1" />
+        <div className="main-banner__data s-center">
+          <p className="t2 s-mb-0">Mi Curso de EDTEAM</p>
+          <p>Debemos terminar el Curso OGT</p>
+          <a href="ed.team" className="button">Siguiente Leccion</a>
+        </div>
+      </div>
     </div>
-  );
-}
+  </div>
+  
+  <div className="ed-grid m-grid-3">
+    <Curso />
+    <Curso />  
+    <Curso />
+    <Curso />  
+    <Curso />
+    <Curso />
+  </div>
+
+  </>
+)
+
 
 export default App;
