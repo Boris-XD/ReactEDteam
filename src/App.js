6610f18a13781024b574dba6ba5becdb2b3ce0f3
seaborisrc@gmail.com <seaborisrc@gmail.com>
2021-02-09 06:32:30 -0400

    Grilla con array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import "./styles/styles.scss";
 import Curso from "./Curso";
 
+const cursos = [
+  {
+    title: "Curso de React",
+    image: "https://edteam-media.s3.amazonaws.com/courses/small/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg",
+    price: 20,
+    teacher: "Beto Quiroga",
+  },
+  {
+    title: "Curso de Docker",
+    image: "https://edteam-media.s3.amazonaws.com/courses/small/e41bbf77-d458-48d9-814b-7dd62a214e40.png",
+    price: 20,
+    teacher: "Beto Quiroga Docker",
+  },
+  {
+    title: "Curso de Ingles",
+    image: "https://edteam-media.s3.amazonaws.com/courses/small/4feb100b-5d47-4daf-b421-9983c5ecdfa8.png",
+    price: 20,
+    teacher: "Beto Quiroga",
+  },
+  {
+    title: "Curso de React",
+    image: "https://edteam-media.s3.amazonaws.com/courses/small/82dfe34a-4e3b-4904-bdb5-03f45112fcd2.png",
+    price: 20,
+    teacher: "Beto Quiroga Wordpress",
+  },
+      
+]
+
+
+
 const App = ()=>(
   <>
     <div className="main-banner img-container l-section" id="main-banner">
@@ -15,18 +45,18 @@ const App = ()=>(
       </div>
     </div>
   </div>
+
+ 
   
   <div className="ed-grid m-grid-3">
-    <Curso />
-    <Curso />  
-    <Curso />
-    <Curso />  
-    <Curso />
-    <Curso />
+    {
+      cursos.map(
+        eachObject => <Curso title={eachObject.title} image={eachObject.image} price={eachObject.price} teacher={eachObject.teacher} />
+      )
+    }
   </div>
 
   </>
 )
-
 
 export default App;
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,29 +1,45 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-const Curso = () =>(
+//const getEdad = edad => edad > 18
+// {`Este Es mi Nombre ${datos.nombre}` }
+// { getEdad(datos.Edad) ? "Es mayor" : "No es Mayor"}
+
+const Curso = ({title, image, price, teacher}) =>(
     <article className="card">
     <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src="https://ux.ed.team/img/curso-portada.png" alt="ed.team" />
+        <img src={image} alt={title} />
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-        <h3 className="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
-        </h3>
         <div className="s-mb-2 s-main-center">
         <div className="card__teacher s-cross-center">
             <div className="card__avatar s-mr-1">
             <div className="circle img-container">
-                <img src="https://ux.ed.team/img/profesor.jpg" alt="ed.team" />
+                <img src="https://ux.ed.team/img/profesor.jpg" alt={teacher} />
             </div>
             </div>
-            <span className="small">Alexys Lozada</span>
+            <span className="small">{teacher}</span>
         </div>
         </div>
         <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="ed.team">$ 20USD</a>
+        <a className="button--ghost-alert button--tiny" href="ed.team">$ {price} USD</a>
         </div>
     </div>
     </article>
 )
+
+Curso.propTypes = {
+    title: PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.number,
+    teacher: PropTypes.string,
+}
+
+Curso.defaultProps = {
+    title: "No se encontro el Titulo", 
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.WClquzREj5wSmuxg_CMIjgHaFM%26pid%3DApi&f=1",
+    price: "--",
+    teacher:"Sin definir",
+}
 
 export default Curso;
